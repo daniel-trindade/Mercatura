ff88699b999a8bd1c74660ef931967d433d9a383
daniel-trindade <danielbrunotrindade@gmail.com>
2024-10-14 18:44:36 -0300

    Components created for sale and consult

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { OnSaleComponent } from './pages/on-sale/on-sale.component';
+import { ConsultComponent } from './pages/consult/consult.component';
 
 export const routes: Routes = [
   {path: "", component: LoginPageComponent},
-  {path: "home", component: HomePageComponent}
+  {path: "home", component: HomePageComponent},
+  {path: "vendas", component: OnSaleComponent},
+  {path: "consulta", component: ConsultComponent}
 ];
