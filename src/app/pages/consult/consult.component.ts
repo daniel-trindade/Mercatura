499af4488252bc847b5e168eca5491471dc906b4
daniel-trindade <danielbrunotrindade@gmail.com>
2024-10-14 19:19:09 -0300

    Consul development

--- src/app/pages/consult/consult.component.ts
+++ src/app/pages/consult/consult.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ConsultFormComponent } from "../../forms/consult-form/consult-form.component";
 
 @Component({
   selector: 'app-consult',
   standalone: true,
-  imports: [],
+  imports: [ConsultFormComponent],
   templateUrl: './consult.component.html',
   styleUrl: './consult.component.scss'
 })
